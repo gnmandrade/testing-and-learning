23f2f280e207ad858c436cd57116d9e60b6549ef
gandrade <90654001+gnmandrade@users.noreply.github.com>
2022-09-17 11:59:06 +0100

    Implemented a bit more of the IB trading API tutorial

--- TradingAPI/IB/IB_API_test.py
+++ TradingAPI/IB/IB_API_test.py
@@ -19,6 +19,11 @@ to test the tool.
 In the initial part of this sample code, the code from the
 following tutorial was used:
 https://www.youtube.com/watch?v=XKbk8SY9LD0
+TODO: Currently implementing this second tutorial
+https://www.youtube.com/watch?v=tWwHNP1Yh1g
+
+Details on the TWS API contracts for getting information:
+https://interactivebrokers.github.io/tws-api/basic_contracts.html
 
 @author: gnmandrade
 """
@@ -26,9 +31,21 @@ https://www.youtube.com/watch?v=XKbk8SY9LD0
 
 # Importing the IB API package
 import ibapi
+# These imports are for the initial connection
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
 
+# For data streaming
+from ibapi.contract import Contract
+
+# To submit orders
+from ibapi.order import *
+
+# To host the data streaming in a different thread
+import threading
+
+# To deal with time data
+import time
 
 # Variables
 
@@ -51,9 +68,54 @@ class Bot():
     ib = None
     def __init__(self):
         # Build connection on init
-        ib = IBApi()
-        ib.connect('127.0.0.1',7497,1)
-        ib.run()
+        self.ib = IBApi()
+        self.ib.connect('127.0.0.1',7497,1)
+        # The run needs to be sent to a different thread
+        # Check run_loop method
+        #ib.run()
+        ib_thread = threading.Thread(target=self.run_loop, deamon=True)
+        ib_thread.start()
         
+        # Sleep to avoid being interrupted by the connect
+        # messages
+        time.sleep(3)
+        
+        # Input symbol to trade
+        symbol = input("Enter the symbol you want to trade: ")
+        
+        # Create Contract object
+        # These are the objects IB uses to stream data
+        # or submit orders
+        contract = Contract()
+        
+        # Set the symbol
+        contract.symbol = symbol.upper()
+        # Define type of symbol
+        # In this case it is a stock
+        contract.secType = 'STK'
+        
+        # Define the exchange
+        # SMART is an automatic selection, but we can force
+        # a given exchange to be used
+        contract.exchange = 'SMART'
+        
+        # Define currency
+        contract.currency = 'USD'
+        
+        # Request Market data
+        # The first argument is the request ID, which
+        # should start with 0 and increase over time as
+        # our application runs
+        # Second arg is our contract object
+        # Third arg has a minimum size of 5
+        #
+        # More info for this method available in:
+        # https://interactivebrokers.github.io/tws-api/classIBApi_1_1EClient.html#a644a8d918f3108a3817e8672b9782e67
+        self.ib.reqRealTimeBars(0, contract, 5, 'TRADES', 1)
+    # Put the run in a different thread
+    def run_loop(self):
+        self.ib.run()
+    
+    
 # Start Bot
 bot = Bot()
