96b898e6f4fd46d1150031024f6957501e2f8342
gandrade <90654001+gnmandrade@users.noreply.github.com>
2024-01-02 15:06:24 +0000

    Finished web data collection example and storage to csv

--- OCR/learning_web_scrapper.py
+++ OCR/learning_web_scrapper.py
@@ -9,8 +9,12 @@ A simple web scrapper following an online course.
 """
 
 # Import libraries
+import csv
 import requests
 from bs4 import BeautifulSoup
+
+# Define wdir
+wdir = '/home/goncalo/Documents/python_projects/0-learning/testing-and-learning/OCR/'
 
 # Define url
 url = "https://www.gov.uk/search/news-and-communications"
@@ -49,3 +53,39 @@ p_class_govuk_body = soup.find_all("p", class_="govuk-body")
 #print(p_class_govuk_body)
 
 
+# Find all titles
+titles_bs = soup.find_all('div', class_='gem-c-document-list__item-title')
+
+# Put titles in a list
+titles_news = [title.find('a').string for title in titles_bs]
+#print(titles_news)
+
+# Print the titles
+#for x in titles_news:
+#    print(x)
+#    print()
+
+# Extract all descriptions
+descriptions_bs = soup.find_all('p', class_='gem-c-document-list__item-description')
+descriptions_news = [description.string for description in descriptions_bs]
+
+# Print the descriptions
+#for x in descriptions_news:
+#    print(x)
+#    print()
+
+# Save data to csv
+headers = ['title', 'description']
+
+# Open a file to write called data.csv
+with open(wdir + 'data.csv', 'w', newline = '') as csvfile:
+    # Create a writer object with the file
+    writer = csv.writer(csvfile, delimiter = ',')
+    writer.writerow(headers)
+    
+    # Loop all elements in titles and descriptions lists
+    for i in range(len(titles_news)):
+        # Create a new row with the title and description for
+        # each point in the loop
+        row = [titles_news[i], descriptions_news[i]]
+        writer.writerow(row)
